e7d8e8c14176b5ee03c48f3425b57b56629b8d4d
Ignacio Beltramino <nachobeltra76@gmail.com>
2025-08-27 20:18:59 -0300

    Update TrabajoPracticoNº1.py

--- TrabajoPracticoNº1.py
+++ TrabajoPracticoNº1.py
@@ -11,6 +11,7 @@
 
 ##nombre = input("Ingrese su nombre: ")
 ##apellido = input("Ingrese su apellido: ")
+##edad = int(input("Ingrese su edad: "))
 ##residencia = input("Ingrese si lugar de residencia: ")
 
 ##print (f"Soy {nombre} {apellido}, tengo {edad} y vivo en {residencia}")
